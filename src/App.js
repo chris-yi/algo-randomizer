4326b52bc3d29abf6a09083d10cd0b70a41c2505
chris-yi <kri5to.yi@gmail.com>
2018-09-03 18:24:07 -0600

    added tree

--- src/App.js
+++ src/App.js
@@ -275,6 +275,16 @@ class App extends Component {
 
           ]
         },
+        {
+          name: 'Tree',
+          instructions:
+            '1) Create a node class.  The constructor should accept an argument that gets assigned to the data property and initialize an empty array for storing children. The node class should have methods "add" and "remove". 2) Create a tree class. The tree constructor should initialize a "root" property to null. 3) Implement "traverseBF" and "traverseDF" on the tree class.  Each method should accept a function that gets called with each element in the tree',
+          image:
+            'https://www.goodfreephotos.com/albums/vector-images/colorful-natural-tree-vector-clipart.png',
+          examples: [
+
+          ]
+        },
       ],
       randomQuestions: [],
       bankClicked: false,
