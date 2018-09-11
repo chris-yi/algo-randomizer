917c8519f881fc7297f1e966d6658bd68a1a97cc
chris-yi <kri5to.yi@gmail.com>
2018-09-10 19:37:53 -0600

    added jewels and stones

--- src/App.js
+++ src/App.js
@@ -427,6 +427,17 @@ class App extends Component {
             'isSubsequence("abc", "acb") // false',
           ]
         },
+        {
+          name: 'Jewels and Stones (Leet Easy)',
+          instructions:
+            'You"re given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels. The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".',
+          image:
+            'https://thumbs.dreamstime.com/z/oval-cut-precious-stones-sparkle-11836670.jpg',
+          examples: [
+            'Input: J = "aA", S = "aAAbbbb" //Output: 3',
+            'Input: J = "z", S = "ZZ" //Output: 0',
+          ]
+        },
       ],
       randomQuestions: [],
       bankClicked: false,
